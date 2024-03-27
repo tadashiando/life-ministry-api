e93a05f2d094f696a48b5e97e38ca6e079337a77
Tadashi Ando <ando.tadashi@gmail.com>
2024-03-27 16:18:46 -0300

    Allow Cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
 
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 
 // #############################################################################
